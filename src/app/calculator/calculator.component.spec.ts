340b92ebcadc31629ab87c8d925fb0cd5ad0d714
Dajin-01 <newdajin@gmail.com>
2026-06-22 13:59:45 +0900

    aligned the build artifact

--- src/app/calculator/calculator.component.spec.ts
+++ src/app/calculator/calculator.component.spec.ts
@@ -9,13 +9,13 @@ describe('CalculatorComponent', () => {
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
-      declarations: [ CalculatorComponent ],
+      declarations: [CalculatorComponent],
       imports: [
         FormsModule,
         ReactiveFormsModule
       ]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
@@ -29,59 +29,49 @@ describe('CalculatorComponent', () => {
   });
 
   it('should calculate sums correctly', () => {
-
-    component.calculatorForm.get('firstInput').setValue(String(10));
-    component.calculatorForm.get('secondInput').setValue(String(2));
-    component.calculatorForm.get('operator').setValue('+');
+    component.calculatorForm.get('firstInput')!.setValue('10');
+    component.calculatorForm.get('secondInput')!.setValue('2');
+    component.calculatorForm.get('operator')!.setValue('+');
     component.onSubmit();
 
     expect(component.result).toBe(12);
-
   });
 
   it('should calculate subtractions correctly', () => {
-
-    component.calculatorForm.get('firstInput').setValue(String(10));
-    component.calculatorForm.get('secondInput').setValue(String(2));
-    component.calculatorForm.get('operator').setValue('-');
+    component.calculatorForm.get('firstInput')!.setValue('10');
+    component.calculatorForm.get('secondInput')!.setValue('2');
+    component.calculatorForm.get('operator')!.setValue('-');
     component.onSubmit();
 
     expect(component.result).toBe(8);
-
   });
 
   it('should calculate divisions correctly', () => {
-
-    component.calculatorForm.get('firstInput').setValue(String(10));
-    component.calculatorForm.get('secondInput').setValue(String(2));
-    component.calculatorForm.get('operator').setValue('/');
+    component.calculatorForm.get('firstInput')!.setValue('10');
+    component.calculatorForm.get('secondInput')!.setValue('2');
+    component.calculatorForm.get('operator')!.setValue('/');
     component.onSubmit();
 
     expect(component.result).toBe(5);
-
   });
 
   it('should calculate multiplications correctly', () => {
-
-    component.calculatorForm.get('firstInput').setValue(String(10));
-    component.calculatorForm.get('secondInput').setValue(String(2));
-    component.calculatorForm.get('operator').setValue('*');
+    component.calculatorForm.get('firstInput')!.setValue('10');
+    component.calculatorForm.get('secondInput')!.setValue('2');
+    component.calculatorForm.get('operator')!.setValue('*');
     component.onSubmit();
 
     expect(component.result).toBe(20);
-
   });
 
   it('should clear the form', () => {
-
-    component.calculatorForm.get('firstInput').setValue(String(10));
-    component.calculatorForm.get('secondInput').setValue(String(2));
-    component.calculatorForm.get('operator').setValue('+');
+    component.calculatorForm.get('firstInput')!.setValue('10');
+    component.calculatorForm.get('secondInput')!.setValue('2');
+    component.calculatorForm.get('operator')!.setValue('+');
     component.onClear();
 
-    expect(component.calculatorForm.get('firstInput').value).toBeNull();
-    expect(component.calculatorForm.get('secondInput').value).toBeNull();
-    expect(component.calculatorForm.get('operator').value).toBe('');
-
+    expect(component.calculatorForm.get('firstInput')!.value).toBeNull();
+    expect(component.calculatorForm.get('secondInput')!.value).toBeNull();
+    expect(component.calculatorForm.get('operator')!.value).toBe('');
   });
 });
